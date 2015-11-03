601a361de8aff3ad4ebae31d705e797f18d618b7
wzaghal <wisamzaghal@gmail.com>
2015-11-02 21:11:24 -0500

    initial commit

--- src/index.js
+++ src/index.js
@@ -1,38 +1,67 @@
 var express = require('express');
 var bodyParser = require('body-parser');
-var rp = require('request-promise');
-var response = require('./hackbot');
 var app = express();
 var port = process.env.PORT || 8080;
 var router = express.Router();
-var webhook = 'https://hooks.slack.com/services/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';
-
+var rp = require('request-promise');
+var token = 'UkWMGR8rpzx37Qyss3JZbEdp';
 
 // body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // test route
 app.get('/', function (req, res) { res.status(200).send('Hello world!') });
-app.post('/hello', response);
-// error handler
+
+app.post('/challenge', function(req, res, next) {
+  // console.log(req);
+  submitChallenge(req.body.user_name, req.body.text);
+  res.status(200).send('OK');
+});
+
+app.post('/accept', function(req, res, next) {
+  var userName = req.body.user_name;
+  var botPayload = {
+    text : 'Let the games begin.',
+    'icon_emoji': ':pingpong:'
+  };
+
+  // avoid infinite loop
+  if (userName !== 'slackbot') {
+    return res.status(200).json(botPayload);
+  } else {
+    return res.status(200).end();
+  }
+});
+
 app.use(function (err, req, res, next) {
   console.error(err.stack);
   res.status(400).send(err.message);
 });
 
-// app.listen(port, function () {
-//   console.log('Slack bot listening on port ' + port);
-// });
+app.listen(port, function () {
+  console.log('Slack bot listening on port ' + port);
+});
 
-var options = {
-  method: 'POST',
-  uri: webhook,
-  body: {
-    "payload": JSON.stringify({text:'lorem'})
-  }
-};
 
-rp(options).then(function(){
-  console.log('yay');
-})
 
+function submitChallenge(challenger, challengee) {
+  var text = challengee + ' is being challenged by ' + challenger + '!\n' +
+  challengee + ', do you accept?';
+
+  var options = {
+      method: 'POST',
+      uri: 'https://hooks.slack.com/services/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',
+      body: {
+        'text': text,
+        'icon_emoji': ':pingpong:'
+      },
+      json: true // Automatically stringifies the body to JSON
+  };
+
+  rp(options).then(function(res) {
+    console.log(res);
+  })
+  .then(null, function(e){
+    console.log(e);
+  })
+}
